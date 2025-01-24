f0e6abb9dc34d8c8965630ba1675900f137c3c8e
Jean Peixoto <jeanpeixoto70@gmail.com>
2025-01-24 11:04:48 -0300

    styling the profile image

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1593720213428-28a5b9e94613?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                 />
             <div className={styles.profile}>
-                <img src="https://avatars.githubusercontent.com/u/147006726?v=4"/>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/147006726?v=4"/>
 
                 <strong>Jean Peixoto</strong>
                 <span>Analista de Sistemas Jr</span>
