aff41ec1bf709f1ffd9241c40092bb41da19cfef
Jean Peixoto <jeanpeixoto70@gmail.com>
2025-01-24 12:28:07 -0300

    styling the commentform

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -24,10 +24,17 @@ export function Post(){
                 <a href="">#novoprojeto </a>{' '}
                 <a href="">#nlw </a>{' '}
                 <a href="">#rocketseat</a>
-            
             </p>
-
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea 
+                    placeholder="Deixe um cometário"
+                />
+
+                <button type="submit">Comentar</button>
+            </form>
 
         </article>
     )
