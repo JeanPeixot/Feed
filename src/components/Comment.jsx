5b1cb0281576c296e2938a152e4492a95fe4f6ff
Jean Peixoto <jeanpeixoto70@gmail.com>
2025-01-24 13:53:25 -0300

    Styling the avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment(){
     return(
         <div className={styles.comment}>
-            <img src="https://avatars.githubusercontent.com/u/147006726?v=4" alt="" />
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/147006726?v=4" alt="" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
