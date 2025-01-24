19630851ed85ac2ecc5489ea1d0acce966abc5a6
Jean Peixoto <jeanpeixoto70@gmail.com>
2025-01-24 09:15:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {Post} from './Post';
+
+ export function App() {
+  return (
+    <div>
+    <Post 
+        author="Jean Peixoto" 
+        content= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus repellat harum unde sapiente sed atque saepe, possimus, dicta nisi fugit tempore magni quaerat maiores fugiat. Debitis corporis dolorum quae perspiciatis."   
+     />
+    <Post
+        author="Romero Brito"
+        content="Um novo comentário"
+    />
+
+    </div>
+  )
+}
+
