5cbc74871fae655d2fb4ec6b0207afd8e8f648c1
Jean Peixoto <jeanpeixoto70@gmail.com>
2025-01-24 15:25:07 -0300

    Inserting a new comment from useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,15 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
+import { useState } from 'react';
+
 
 export function Post({ author , publishedAt, content}){
+
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -16,6 +23,15 @@ export function Post({ author , publishedAt, content}){
         locale: ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment(){
+        event.preventDefault()
+
+
+        setComments([...comments, comments.length + 1]);
+        
+
+    }
 
     return (
         <article className={styles.post}>
@@ -44,7 +60,7 @@ export function Post({ author , publishedAt, content}){
            
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea 
                     placeholder="Deixe um cometário"
@@ -55,9 +71,9 @@ export function Post({ author , publishedAt, content}){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
 
         </article>
