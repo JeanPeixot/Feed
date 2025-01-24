5cc14c8800a8424e644313c962c86babe950cadf
Jean Peixoto <jeanpeixoto70@gmail.com>
2025-01-24 10:30:12 -0300

    styling the sidebar and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { Header } from './components/Header';
-import {Post} from './Post';
+import { Header } from "./components/Header";
+import { Post } from "./Post";
 
-import './global.css';
+import styles from "./App.module.css";
 
- export function App() {
+import "./global.css";
+
+export function App() {
   return (
     <div>
       <Header />
 
-    <Post 
-        author="Jean Peixoto" 
-        content= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus repellat harum unde sapiente sed atque saepe, possimus, dicta nisi fugit tempore magni quaerat maiores fugiat. Debitis corporis dolorum quae perspiciatis."   
-     />
-    <Post
-        author="Romero Brito"
-        content="Um novo comentário"
-    />
-
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post
+            author="Jean Peixoto"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus repellat harum unde sapiente sed atque saepe, possimus, dicta nisi fugit tempore magni quaerat maiores fugiat. Debitis corporis dolorum quae perspiciatis."
+          />
+          <Post author="Romero Brito" 
+          content="Um novo comentário" />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
-
